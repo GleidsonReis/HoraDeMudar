b381276cd60300a87810db78fdf125d3f64a2ad6
iftm <iftm@lab02-mXX.laboratorios>
2018-03-02 21:06:36 -0300

    Commit Inicial

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <title>Hello, world!</title>
+    </head>
+    <body>
+		<?php
+			echo "ALloou";
+		?>
+        <div class="container">
+            <div class="row">
+                <h1>Hello, world Muuuundo!</h1>
+            </div>
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.slim.min.js"></script>
+        <script src="js/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    </body>
+</html>
