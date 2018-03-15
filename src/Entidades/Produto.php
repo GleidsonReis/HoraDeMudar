f7e9d8659cc7b3bb28039ca6b0de4fa819acb739
iftm <iftm@labXX-mXX.laboratorios>
2018-03-14 22:30:02 -0300

    //Atualizado com Entidades

---
+++ src/Entidades/Produto.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Produto
+ *
+ * @author iftm
+ */
+namespace HoraDeMudar\Entidades;
+class Produto {
+    public function msg(){
+        echo "Mensagem da Função";
+    }
+}
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,25 +1,7 @@
-<!doctype html>
-<html lang="pt-br">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-
-        <title>Hello, world!</title>
-    </head>
-    <body>
 	<?php
-
+namespace HoraDeMudar;
+use HoraDeMudar\Entidades\Produto;
 echo "Olá do Bootstrap!!!"
 
+Produto: ;
 ?>	
-        
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="js/jquery-3.2.1.slim.min.js"></script>
-        <script src="js/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    </body>
-</html>
