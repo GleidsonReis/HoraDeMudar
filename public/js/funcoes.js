37cfd8932ca6acfd0b564206ded5074dad54d103
iftm <iftm@labXX-mXX.laboratorios>
2018-04-25 22:28:13 -0300

    // Incluido js e ajax

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,19 +1,27 @@
-    function ajax() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("div_retorno").innerHTML = this.responseText;
-            }
-        };
-        xhttp.open("GET", "texto.html", true);
-        xhttp.send();
-    }
-
+//    function ajax() {
+//        var xhttp = new XMLHttpRequest();
+//       xhttp.onreadystatechange = function () {
+//            if (this.readyState == 4 && this.status == 200) {
+ //               document.getElementById("div_retorno").innerHTML = this.responseText;
+ //           }
+   //     };
+     //   xhttp.open("GET", "texto.html", true);
+       // xhttp.send();
+//    }
+//
 $(document).ready(function(){
    $("#botao1").click(function(){
-       alert('Heelloo Hoooorrlld')
-       
-       
+       $.ajax({
+           type:'GET',
+           url: '/texto.html',
+           data:"",
+           success: function(dados){
+               $("div_retorno").html(dados);
+           }
+           
+           
+       });
+           
    }); 
     
     
