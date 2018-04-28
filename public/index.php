03ff04527d9cd96d437b59c3d1d2e37ceab95821
iftm <iftm@labXX-mXX.laboratorios>
2018-04-27 22:28:39 -0300

    // Atualizado Ajax e Jquery

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 
 
 <?php
+ini_set('display_erros', true);
+ini_set('display_startup_erros',true);
+error_reporting(E_ALL);
 		require __DIR__.'/../src/bootstrap.php';
                 
 		?>
--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -10,25 +10,32 @@
 //    }
 //
 $(document).ready(function(){
-   $("#botao1").click(function(){
+   $('#botao1').click(function(){
        $.ajax({
            type:'GET',
            url: '/texto.html',
            data:"",
+           
            success: function(dados){
-               $("div_retorno").html(dados);
-           }
-           
-           
+               $("#div_retorno").html(dados);
+           },
+           beforeSend: function (){
+               $('#processando').css({display:"block"});
+           },
+           complete: function (){
+               setTimeout(function(){
+                   $('#processando').css({display:"none"});
+               }, 5000);
+           },
+           error: function (){
+               $('#div_retorno').html("Erro em chamar a função");
+               setTimeout(function(){
+                   $('#div_retorno').css({display:"none"});
+               }, 5000);
+         }  
        });
            
    }); 
     
-    
-    
-    
-    
-    
-    
-    
+     
 });
